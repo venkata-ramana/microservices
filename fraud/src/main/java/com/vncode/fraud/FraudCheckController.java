07655eb540f8ba0d023a72f57c439a0bef21678a
Venkataramana Neelapala <vramana98@gmail.com>
2022-01-16 13:27:40 +0530

    add eureka server for communication between services

--- fraud/src/main/java/com/vncode/fraud/FraudCheckController.java
+++ fraud/src/main/java/com/vncode/fraud/FraudCheckController.java
@@ -1,6 +1,7 @@
 package com.vncode.fraud;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,12 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("api/v1/fraud-check")
 @AllArgsConstructor
+@Slf4j
 public class FraudCheckController {
     private final FraudCheckService fraudCheckService;
 
     @GetMapping(path = "{customerId}")
     public FraudCheckResponse isFraudster(@PathVariable("customerId") Integer customerId){
         boolean isFraudlentCustomer = fraudCheckService.isFradulentCustomer(customerId);
+        log.info("Fraud check request for customer {}", customerId);
         return new FraudCheckResponse(isFraudlentCustomer);
     }
 }
